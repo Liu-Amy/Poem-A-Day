830b4dccd3b9b67d027a8f99928976c6bc12bd7a
Adithya Nott <anott3@gatech.edu>
2017-10-31 17:57:52 -0400

    ayyy let's go

--- extension/script.js
+++ extension/script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  getPoetry();
-});
-//Randomly selects a poem. Yeah. This simple.
-function getPoetry() {
-    var xhr = new XMLHttpRequest();
-    entries = null;
-    url = 'http://poetrydb.org/author/*';
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            entries = JSON.parse(this.responseText);
-            entry = entries[Math.floor(Math.random()*entries.length)];
 
+    chrome.storage.local.get("poems", function(poemsObj) {
+
+        var poems = poemsObj.poems;
+        if (typeof poems === "undefined") {
+            //need to initialize
+
+            getPoetry(true);
+
+        } else {
+            //was already initialized :D
+            entry = poems[Math.floor(Math.random()*poems.length)];
             title = entry.title;
             lines = entry.lines;
 
@@ -19,11 +19,45 @@ function getPoetry() {
                 final_string += lines[i] + '\n';
             }
 
-            final_string += '\n' + entry.author;
+            final_string += '\n-' + entry.author;
             document.getElementById("title").innerHTML = title;
             document.getElementById("poetry").innerHTML = final_string;
+            getPoetry(false);
+        }
+    });
+});
+
+//Randomly selects a poem. Yeah. This simple.
+function getPoetry(initialload) {
+    var xhr = new XMLHttpRequest();
+    entries = null;
+    url = 'http://poetrydb.org/author/*';
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            entries = JSON.parse(this.responseText);
+            short_poems = []
+            for (i = 0 ; i < entries.length; i++) {
+                if (entries[i].linecount <= 30) {
+                    short_poems.push(entries[i]);
+                }
+            }
+            chrome.storage.local.set({"poems": short_poems}, function() {});
+            if (initialload) {
+                entry = short_poems[Math.floor(Math.random()*short_poems.length)];
+                title = entry.title;
+                lines = entry.lines;
+
+                final_string = "";
+                for (i = 0; i < entry.linecount; i++) {
+                    final_string += lines[i] + '\n';
+                }
+
+                final_string += '\n- ' + entry.author;
+                document.getElementById("title").innerHTML = title;
+                document.getElementById("poetry").innerHTML = final_string;
+            }
         }
     };
-    xhr.open('GET', url, true);
+    xhr.open('GET', url);
     xhr.send();
 }
